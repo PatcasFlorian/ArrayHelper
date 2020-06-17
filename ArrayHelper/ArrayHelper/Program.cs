caf6b2126a895dcc18e709bc6d386bf111455454
Patcas Florian <patcasf12@gmail.com>
2020-06-17 16:30:00 +0300

    Assignment1

--- ArrayHelper/ArrayHelper/Program.cs
+++ ArrayHelper/ArrayHelper/Program.cs
@@ -9,11 +9,14 @@ namespace ArrayHelper
         {
             int index;
             int element = 15;
+            string element1 = "Ana";
             int[] nr = new int[] { 3, 5, 23, 12 };
-            
+            string[] str = new string[] { "Ana", "are", "mere" };
             
             index = ArrayHelper.ArrayHelper<int>.IndexReturn(nr, element);
             Console.WriteLine($"Elementul {element} , are indexul : {index}");
+            index = ArrayHelper.ArrayHelper<string>.IndexReturn(str, element1);
+            Console.WriteLine($"Elementul {element1} , are indexul : {index}");
         }
     }
 }
